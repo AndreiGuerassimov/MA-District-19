921f6725f38160770efce955022f507749262f61
Andre <andrei.guer@gmail.com>
2025-10-30 18:56:35 -0400

    blocksy theme installation

--- wp-config.php
+++ wp-config.php
@@ -100,3 +100,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+
+/** Prevent offical WordPress theme downloads. */
+define('CORE_UPGRADE_SKIP_NEW_BUNDLED', true);
