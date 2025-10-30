c83b93b02c4ee043ff0a35d2209a5bd657e24db2
Andre <andrei.guer@gmail.com>
2025-10-30 19:15:00 -0400

    contact form 7 and cf7 antispam installed

---
+++ wp-content/plugins/cf7-antispam/build/admin-scripts.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'fd471df139a3fc9a4f1f');
---
+++ wp-content/plugins/cf7-antispam/build/script.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '1829dacfa96318b4e905');
